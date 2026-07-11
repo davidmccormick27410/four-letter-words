c84f4b5ac5fa3fc2f9f6fcf3434b4148a014e573
David McCormick <davidmccormick27410@gmail.com>
2026-07-11 19:11:11 -0400

    Fixed file_path lookup in Validator

--- four_letter_words/check_inputs.py
+++ four_letter_words/check_inputs.py
@@ -1,49 +1,65 @@
-"""
-Functions that validate the input
+from os import path
 
-1. Is the submitted word four letters long?
-2. Is the submitted word in the file list?
-3. Does the submitted word differ from the previous submitted word by only one letter?
-"""
 
-def input_is_in_file_list(word_list,submitted_word: str) -> bool:
+class Validator():
     """
-    :type submitted_word: str
-    :param word_list: a dataframe
-    :param submitted_word: 
-    :return: 
+    Functions that validate the input
+
+    1. Is the submitted word four letters long?
+    2. Is the submitted word in the file list?
+    3. Does the submitted word differ from the previous submitted word by only one letter?
     """
-    if submitted_word not in word_list:
+    def __init__(self):
+        basedir = path.dirname(path.abspath(__file__))
+        self.file_path = path.join(basedir, 'data\\four_letter_words.txt')
+        self.word_list = ""
+        self.the_previous_word = ""
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as file_object:
+                # Perform file operations here
+                self.word_list = file_object.read().splitlines()
+        except FileNotFoundError:
+            print("File not found.")
+        except:
+            print("Something went wrong.")
+
+        self.the_previous_word = ""
+
+    def input_is_four_letters_long(submitted_word: str) -> bool:
+        """
+
+        :param submitted_word:
+        :return:
+        """
+        return len(submitted_word) == 4
+
+    def input_is_in_file_list(self, submitted_word: str) -> bool:
+        """
+        :type submitted_word: str
+        :param submitted_word:
+        :return:
+        """
+        if submitted_word not in self.word_list:
+            return False
+        return True
+
+    def input_is_submitted_word_only_off_by_one_from_the_previous_word(
+            self,submitted_word):
+        """
+        :param submitted_word:
+        :return:
+        """
+        if self.the_previous_word == "":
+            return True
+        matches = 0
+        if submitted_word[0] == self.the_previous_word[0]:
+            matches += 1
+        if submitted_word[1] == self.the_previous_word[1]:
+            matches += 1
+        if submitted_word[2] == self.the_previous_word[2]:
+            matches += 1
+        if submitted_word[3] == self.the_previous_word[3]:
+            matches += 1
+        if matches == 3:
+            return True
         return False
-    return True
-
-def input_is_four_letters_long(submitted_word: str) -> bool:
-    """
-    
-    :param submitted_word: 
-    :return: 
-    """
-    return len(submitted_word) == 4
-
-def input_is_submitted_word_only_off_by_one_from_the_previous_word(
-        the_previous_word,submitted_word):
-    """
-    :param the_previous_word:
-    :param submitted_word: 
-    :return: 
-    """
-    if the_previous_word == "":
-        return True
-    matches = 0
-    if submitted_word[0] == the_previous_word[0]:
-        matches += 1
-    if submitted_word[1] == the_previous_word[1]:
-        matches += 1
-    if submitted_word[2] == the_previous_word[2]:
-        matches += 1
-    if submitted_word[3] == the_previous_word[3]:
-        matches += 1
-
-    if matches == 3:
-        return True
-    return False
