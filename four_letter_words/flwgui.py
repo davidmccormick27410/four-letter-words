c46653ada9c754b42913fc49fa1597d70b209051
David McCormick <davidmccormick27410@gmail.com>
2026-08-06 17:19:04 -0400

    Added remove last word button and remove_word_from_list function

--- four_letter_words/flwgui.py
+++ four_letter_words/flwgui.py
@@ -10,31 +10,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.validator = Validator()
         self.previous_word = ""
-        self.pushButton.pressed.connect(self.handle_button_pressed)
-        self.lineEdit.returnPressed.connect(self.handle_button_pressed)
+        self.btn_add_word.pressed.connect(self.add_word_to_list)
+        self.btn_remove_last_word.pressed.connect(self.remove_word_from_list)
+        self.lineEdit.returnPressed.connect(self.add_word_to_list)
 
-    def handle_button_pressed(self):
+    def add_word_to_list(self):
         self.submitted_word = self.lineEdit.text()
         if self.validator.input_is_four_letters_long(self.submitted_word):
-            if self.validator.input_is_in_file_list(self.submitted_word.upper()) :
-                if self.validator.input_is_submitted_word_only_off_by_one_from_the_previous_word(self.submitted_word.upper(), self.previous_word):
-                    self.listWidget.addItem(self.submitted_word.upper())
-                    self.previous_word = self.submitted_word.upper()
-                    self.lineEdit.clear()
-                    self.listWidget.scrollToBottom()
-                    return
+            if self.validator.submitted_word_is_not_already_in_list(self.submitted_word, self.previous_word):
+                if self.validator.input_is_in_file_list(self.submitted_word.upper()) :
+                    if self.validator.input_is_submitted_word_only_off_by_one_from_the_previous_word(self.submitted_word.upper()):
+                        self.listWidget.addItem(self.submitted_word.upper())
+                        self.previous_word = self.submitted_word.upper()
+                        self.lineEdit.clear()
+                        self.listWidget.scrollToBottom()
+                        return
+                    else:
+                        QMessageBox.information(self, "Nope", "Your word is not only off by one letter from {self.previous_word}")
+                        self.lineEdit.clear()
+                        return
                 else:
-                    QMessageBox.information(self, "Nope", "Your word is not only off by one letter from {self.previous_word}")
+                    QMessageBox.information(self, "Nope", "Your word is not in the word list! Take it up with Scrabble, if you don't like it.")
                     self.lineEdit.clear()
                     return
-            else:
-                QMessageBox.information(self, "Nope", "Your word is not in the word list! Take it up with Scrabble, if you don't like it.")
-                self.lineEdit.clear()
-                return
         else:
             QMessageBox.information(self, "Nope", "Your word is not four letters long!")
             self.lineEdit.clear()
             return
+
+    def remove_word_from_list(self):
+        self.listWidget.takeItem(self.listWidget.count() - 1)
+
 
 
 if __name__ == "__main__":
